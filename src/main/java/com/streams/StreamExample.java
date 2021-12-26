14526e5210956bdebea71e7cbf914735f2769e2a
Marwan <marwan_subhi2002@yahoo.com>
2021-12-25 23:48:09 -0500

    Added stream package

--- src/main/java/com/streams/StreamExample.java
+++ src/main/java/com/streams/StreamExample.java
@@ -1,5 +1,9 @@
-package com.streams;/**
-Created by Marwan on 12/25/2021 around 11:42 PM
-*/
+package com.streams;
+
+/**
+ * Created by Marwan on 12/25/2021 around 11:42 PM
+ */
 public class StreamExample {
+
+
 }
